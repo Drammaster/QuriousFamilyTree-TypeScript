c1152babc2d3b210977ecda78bb568b7b8285f14
drammaster2 <Csabi1996@windowslive.com>
2020-12-23 12:23:37 +1300

    Some visuals are done. Issues with handeling childrens

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const familyTreeTest = [
+    {
+      id: 1,
+      name: "Dad",
+      children: [3, 4],
+      gender: "male",
+      parents: [],
+    },
+    {
+      id: 2,
+      name: "Mom",
+      children: [3, 4],
+      gender: "female",
+      parents: [],
+    },
+    {
+      id: 3,
+      name: "Child1",
+      children: [],
+      gender: "female",
+      parents: [1, 2],
+    },
+    {
+      id: 4,
+      name: "Child2",
+      children: [],
+      gender: "male",
+      parents: [1, 2],
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FamilyMember tree={familyTreeTest}></FamilyMember>
+  );
+}
+
+function Card(props) {
+  return (
+  <div className="card" style={ props.gender === "male" ? {backgroundColor: "lightblue"} : {backgroundColor: "lightpink"}}>{props.name}</div>
+  )
+}
+
+function FamilyMember(props) {
+  const parentsToAdd = []
+  let childrenToFind = []
+
+  for(const value of props.tree) {
+    if(value.parents.length === 0) {
+      parentsToAdd.push(value)
+      parentsToAdd.push(props.tree.find(obj =>  {
+        return obj.gender !== value.gender && JSON.stringify(value.children.sort()) === JSON.stringify(obj.children.sort())  
+      } ))
+      break;
+    }
+  }
+
+  for(const tester of parentsToAdd[0].children) {
+    childrenToFind.push(tester);
+  }
+  
+  return (
+    <div>
+      {parentsToAdd.map((value) => {
+        return <Card name={value.name} gender={value.gender}/>
+      })}
+
+      <hr></hr>
+
+      {childrenToFind.map((value) => {
+        childrenToFind.push(props.tree.find(obj =>  {
+          return obj.id === value 
+        } ))
+        return <Card name={props.tree.find(obj =>  { return obj.id === value } ).name} gender={props.tree.find(obj =>  { return obj.id === value } ).gender}/>
+      })}
     </div>
   );
 }
